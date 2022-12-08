48a1737dc326347a5d9a6b53ef34d5fe0b517a05
NzzyYkky <62602398+NzzyYkky@users.noreply.github.com>
2022-12-08 10:48:00 +0900

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+//　必要なのはpuppeteerとfs
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+	const browser = await puppeteer.launch({
+		headless: false,
+	});
+	const page = (await browser.pages())[0];
+	// テキストを抽出するURLを指定。　ローカルホストの場合は立ち上げておくこととhttp://から始めること
+	await page.goto('http://localhost:3000/');
+	//　$$evalメソッドで第1引数にタグやクラス名、第2引数に関数を指定。
+	const paragraph = await page.$$eval('p', (elements) => {
+		// テキストをpタグずつテキストへ挿入していく。
+		return elements.map((e) => e.innerText);
+	});
+
+	// 配列だから要素を取得してくる
+	await paragraph.forEach((el) => {
+		// それをテキストファイルに追記だ！！　+ '\n'にしないと改行が入らない
+		fs.appendFile('./test.txt', el + '\n', (err) => {
+			if (err) throw err;
+		});
+	});
+
+	await page.goto('http://localhost:3000/');
+	//　$$evalメソッドで第1引数にタグやクラス名、第2引数に関数を指定。
+	const paragraph02 = await page.$$eval('p', (elements) => {
+		// テキストをpタグずつテキストへ挿入していく。
+		return elements.map((e) => e.innerText);
+	});
+
+	// 配列だから要素を取得してくる
+	await paragraph02.forEach((el) => {
+		// それをテキストファイルに追記だ！！　+ '\n'にしないと改行が入らない
+		fs.appendFile('./test.txt', el + '\n', (err) => {
+			if (err) throw err;
+		});
+	});
+
+	await browser.close();
+})();
